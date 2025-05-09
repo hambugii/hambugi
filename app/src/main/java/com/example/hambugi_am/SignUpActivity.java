b2122788f62f44068598a374ecd4adaa42adba00
HBAKM <star135058@naver.com>
2025-05-10 00:02:20 +0900

    0510

--- app/src/main/java/com/example/hambugi_am/SignUpActivity.java
+++ app/src/main/java/com/example/hambugi_am/SignUpActivity.java
@@ -1,12 +1,22 @@
 package com.example.hambugi_am;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);  // 회원가입 화면
+        setContentView(R.layout.activity_sign_up);
+
+        // 돌아가기 TextView 찾기
+        TextView backButton = findViewById(R.id.backButton);
+
+        // 현재 액티비티 종료
+        backButton.setOnClickListener(v -> finish());
     }
 }
